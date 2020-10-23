cddec96459fb28fad0e41e3df036c1dbeea0979f
galmeid <53835830+galmeid@users.noreply.github.com>
2020-10-23 13:50:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ GOOD_NIGHT_QUOTES = ["Good night!", "Hope you had an amazing day!", "I'm going t
 
 
 def automate_texts(texts=GOOD_NIGHT_QUOTES):
-    twilio_account = "AC1512436533a410a49c5dda4a74c67e42"
-    auth_token = "8ef2622a34500bbb8a245e3daa0cfea8"
+    twilio_account = #Your twilio account
+    auth_token = # Your twilio authentication token
     client = Client(twilio_account, auth_token)
     text = texts[random.randint(0, len(texts))]
-    client.messages.create(to=str(7657753709), from_=str(3145396409), body=text)
+    client.messages.create(to=str(#phone number to send message to), from_=str(your phone number), body=text)
 
 schedule.every().day.at("22:30").do(automate_texts, GOOD_NIGHT_QUOTES)
